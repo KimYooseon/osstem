d1eff0d5b06fb68a43f12cb434d3b927dbc6a922
KimYooseon <smir1997@naver.com>
2022-12-20 20:32:54 +0900

    1220
    opengl

---
+++ opengl_1220_practice2/widget.cpp
@@ -0,0 +1,158 @@
+#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QOpenGLWidget(parent)
+{
+    rectPosX = 0.0f;
+    rectPosY = 0.0f;
+    rectSize = 250;
+
+    xStep = 5.0f;
+    yStep = 5.0f;
+
+    setWindowTitle("Hello OpenGL");
+    resize(600, 600);
+
+    QTimer *timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), SLOT(timerFunction()));
+    timer->start(1000/60);
+}
+
+Widget::~Widget()
+{
+}
+
+void Widget::initializeGL()
+{
+    initializeOpenGLFunctions();
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+void Widget::resizeGL(int w, int h)
+{
+    windowWidth = w;
+    windowHeight =h;
+
+    glViewport(0,0, (GLint)w, (GLint)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
+void Widget::paintGL()
+{
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    glColor3f(1.0, 1.0, 1.0);
+
+//    glBegin(GL_POLYGON);
+//    glVertex2f(0.0, 0.6);
+//    glColor3f(1.0, 0.0, 0.0);
+//    glVertex2f(-0.6, 0.0);
+//    glVertex2f(-0.4, -0.6);
+//    glColor3f(0.0, 1.0, 0.0);
+//    glVertex2f(0.4, -0.6);
+//    glColor3f(0.0, 0.0, 1.0);
+//    glVertex2f(0.6, 0.0);
+
+
+//    glPointSize(10.0);
+//    glEnable(GL_POINT_SMOOTH);  //점 네모난 부분 동그랗게 만들어주는 방법
+//    glBegin(GL_POINTS);
+//    glVertex2f(0.0, 0.5);
+//    glColor3f(1.0, 0.0, 0.0);
+
+//    glVertex2f(-0.5, -0.5);
+//    glVertex2f(0.5, -0.5);
+
+//    glBegin(GL_LINES);
+//    GLfloat x = -0.8;
+//    GLfloat y = 0.4;
+//    for(int i=0; i<6; i++){
+//        glVertex2f(x, y);
+//        x+=0.3;
+//        y*=-1;
+//    };
+
+//    double rad = 0.5;
+//    glBegin(GL_LINE_LOOP);
+//    for(int i=0; i<360; i++){
+//        double angle, x, y;
+//        angle = i*3.141592 / 180;
+//        x = rad*cos(angle);
+//        y = rad*sin(angle);
+//        glVertex2f(x, y);
+//    }
+
+//    GLfloat w = 1, y;
+//    for(y=0.8; y>-0.8;y-=0.2){
+//        glLineWidth(w++);
+//        glBegin(GL_LINES);
+//        glVertex2f(-0.8, y);
+//        glVertex2f(0.8, y);
+
+//        glEnd();
+//    };
+
+
+    GLushort arPat[] = {0xaaaa, 0xaaaa, 0xaaaa, 0xaaaa, 0x33ff, 0x33ff, 0x33ff, 0x57ff, 0x57ff};
+    GLint arFac[] = {1,2,3,4,1,2,3,1,2};
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+
+    glEnable(GL_LINE_STIPPLE);
+    GLfloat y;
+    GLint idx = 0;
+    for(y = 0.8; y>-0.8; y -= 0.2){
+        glLineStipple(arFac[idx], arPat[idx]);
+        glBegin(GL_LINES);
+        glVertex2f(-0.8, y);
+        glVertex2f(0.8, y);
+        glEnd();
+        idx++;
+    };
+    glEnd();
+
+    glFlush();
+}
+
+void Widget::timerFunction()
+{
+    if(rectPosX > windowWidth - rectSize || rectPosX < -windowWidth)
+        xStep = -xStep;
+
+    if(rectPosY > windowHeight || rectPosY < -windowHeight + rectSize)
+        yStep = -yStep;
+
+    rectPosX += xStep;
+    rectPosY += yStep;
+
+    if(rectPosX > (windowWidth - rectSize + xStep))
+        rectPosX = windowWidth-rectSize-1;
+    else if(rectPosX < -(windowWidth + xStep))
+        rectPosX = -windowWidth-1;
+
+    if(rectPosY > (windowHeight + yStep))
+        rectPosY = windowHeight-1;
+    else if(rectPosY < -(windowHeight - rectSize + yStep))
+        rectPosY = -windowHeight + rectSize -1;
+
+    repaint();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
